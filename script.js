b3a284ba333b758f6acd57395a0627eb8452285f
hugofriasmtz <a3521110393@alumno.uttehuacan.edu.mx>
2024-10-21 18:09:31 -0600

    :hammer: update script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
  
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
         .then(registration => {
             console.log('Service Worker registrado con éxito:', registration);
         })
